207a64e179090d8d3c87859f5a2e71b2ada65e57
Balaji <reachbalajis@gmail.com>
2015-12-03 23:06:27 +0530

    Initial commit

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Sony on 03-12-2015.
+ */
+@SpringBootApplication
+public class MainController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainController.class, args);
+    }
+}
---
+++ src/main/java/com/example/controller/FirstController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Balaji on 03-12-2015.
+ */
+@RestController
+public class FirstController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "Hello.. Its First Spring boot Trial";
+    }
+
+    @RequestMapping(value = "/hello/{firstName}")
+    public String hello(@PathVariable("firstName") String firstName){
+        return "Hello "+firstName+".. Its First Spring boot Trial";
+    }
+}
